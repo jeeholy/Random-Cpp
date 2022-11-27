4e3d0cc90e598601d7ab2f6a36726201c7f7de04
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-27 10:53:08 +0700

    Update ตีกรอบ.c

--- ตีกรอบ.c
+++ ตีกรอบ.c
@@ -2,26 +2,25 @@
 
 int main()
 {
-  int n;
-  scanf("%d", &n);
-  int i=0;
-  for(i; i<n; i++)
-    printf("#");
-  int j=0;
-  for(j; j<n-2; j++)
-  {
-    printf("\n#");
-	  int k=0;
-    for( k; k<n-2;k++)
-      printf(" ");
-    printf("#");
-  }
-  printf("\n");
-  i=0;
-  for(i; i<n; i++){
-    if(n==1) break; 
-    printf("#");
-  }
-   
+  	int n;
+  	scanf("%d", &n);
+  	int i=0;
+  	for(i; i<n; i++)
+    		printf("#");
+  	int j=0;
+  	for(j; j<n-2; j++)
+  	{
+    		printf("\n#");
+		int k=0;
+    		for( k; k<n-2;k++)
+      			printf(" ");
+    		printf("#");
+  	}
+  	printf("\n");
+  	i=0;
+  	for(i; i<n; i++){
+    		if(n==1) break; 
+    		printf("#");
+  	}
   return 0;
 }
