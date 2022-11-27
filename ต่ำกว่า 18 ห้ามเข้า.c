6cf8f658fe890bb67376456ae934ba77bfea3246
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-27 10:34:44 +0700

    Update and rename ต่ำกว่า 18 ห้ามเข้า to ต่ำกว่า 18 ห้ามเข้า.c

--- ต่ำกว่า 18 ห้ามเข้า.c
+++ ต่ำกว่า 18 ห้ามเข้า.c
@@ -7,7 +7,7 @@ int main()
   gets(name);
   scanf("%d", &age);
   if(2022-age >= 18)
-		printf("Welcome %s to NongGipsy Pub", name);
+    printf("Welcome %s to NongGipsy Pub", name);
   else
     printf("You shall not pass!");
 	return 0;
