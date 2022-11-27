631d6f09a273510216945a647f046dc22e19d129
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-27 10:55:14 +0700

    Update and rename ทั้งคู่อายุเท่าไรกันแน่ to ทั้งคู่อายุเท่าไรกันแน่.c

--- ทั้งคู่อายุเท่าไรกันแน่.c
+++ ทั้งคู่อายุเท่าไรกันแน่.c
@@ -3,9 +3,8 @@
 int main()
 {
 	int x, n, m, y;
-  scanf("%d %d %d", &n, &m, &y);
-  
-  x = (m*n+m*y-y)/(m-1);
+  	scanf("%d %d %d", &n, &m, &y);
+  	x = (m*n+m*y-y)/(m-1);
 	printf("%d %d",x , x-n);
 	return 0;
 }
