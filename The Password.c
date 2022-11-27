5dfe6db908ac41764c63fa42f385120e775d1857
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-27 10:46:49 +0700

    Update The Password.c

--- The Password.c
+++ The Password.c
@@ -4,22 +4,25 @@
 int main()
 {
 	char a[45];
-  scanf("%s", &a);
+	scanf("%s", &a);
   
-  int cntA, cnta, cntn, cntp;
-  cntA = cnta = cntn = cntp = 0;
+  	int cntA, cnta, cntn, cntp;
+  	cntA = cnta = cntn = cntp = 0;
   
-  if(strlen(a)>=3 && strlen(a)<=20){
-    int len = strlen(a);
-    for(int i=0; i<len; i++){
-      if(a[i]>64 && a[i]<91) cntA++;
-      else if(a[i]>96 && a[i]<123) cnta++;
-      else if(a[i]>47 && a[i]<58) cntn++;
-      else cntp++;
-    }
-    if(cntA>0 && cnta>0 && cntn>0 && cntp>0) printf("Valid");
-    else printf("Invalid");
-  }
-  else printf("Invalid");
+  	if(strlen(a)>=3 && strlen(a)<=20){
+    		int len = strlen(a);
+    		for(int i=0; i<len; i++){
+      			if(a[i]>64 && a[i]<91) cntA++;
+      			else if(a[i]>96 && a[i]<123) cnta++;
+      			else if(a[i]>47 && a[i]<58) cntn++;
+      			else cntp++;
+    		}
+    		if(cntA>0 && cnta>0 && cntn>0 && cntp>0) 
+			printf("Valid");
+    		else 
+			printf("Invalid");
+  	}
+  	else 
+		printf("Invalid");
 	return 0;
 }
