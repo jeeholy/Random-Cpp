78afcde0604706fd5be437bec251d0482e1d9070
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-27 10:51:57 +0700

    Update จุดกำเนิดเด็กหญิงสมจริง.c

--- จุดกำเนิดเด็กหญิงสมจริง.c
+++ จุดกำเนิดเด็กหญิงสมจริง.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main()
-{ int i, a;
-  for(i=1;i<7;i++){
-    for(a=1;a<=i;a++)
-	    printf("*");
-    printf("\n");
-   }
+{ 
+	int i, a;
+  	for(i=1;i<7;i++){
+    		for(a=1;a<=i;a++)
+	    		printf("*");
+    		printf("\n");
+   	}
 	return 0;
 }
