dd27a51ce69223a1ae14bc19a1d86e977fb2096b
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-27 10:48:13 +0700

    Update กุมภาที่มีไม่เท่ากัน.cpp

--- กุมภาที่มีไม่เท่ากัน.cpp
+++ กุมภาที่มีไม่เท่ากัน.cpp
@@ -3,17 +3,18 @@ using namespace std;
 
 
 int main()
-{ int a;
+{
+	int a;
 	cin >> a;
-  if(a==0)
-	  cout << "Leap Year" ;
-	else if(a%400==0)
-	  cout << "Leap Year" ;
-  else if(a%100==0)
-    cout << "Not a Leap Year" ;
-  else if(a%4==0)
-	  cout << "Leap Year" ;
-  else 
-    cout << "Not a Leap Year" ;
+  	if(a == 0)
+		cout << "Leap Year" ;
+	else if(a%400 == 0)
+	  	cout << "Leap Year" ;
+  	else if(a%100 == 0)
+    		cout << "Not a Leap Year" ;
+  	else if(a%4 == 0)
+		cout << "Leap Year" ;
+  	else 
+    		cout << "Not a Leap Year" ;
 	return 0;
 }
