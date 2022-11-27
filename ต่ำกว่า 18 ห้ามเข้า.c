5d4ad49ec58383619fcf7fc640ad168b5f9d7b77
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-27 10:54:10 +0700

    Update ต่ำกว่า 18 ห้ามเข้า.c

--- ต่ำกว่า 18 ห้ามเข้า.c
+++ ต่ำกว่า 18 ห้ามเข้า.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-  int age;
-  char name[50];
-  gets(name);
-  scanf("%d", &age);
-  if(2022-age >= 18)
-    printf("Welcome %s to NongGipsy Pub", name);
-  else
-    printf("You shall not pass!");
+	int age;
+  	char name[50];
+  	gets(name);
+  	scanf("%d", &age);
+  	if(2022-age >= 18)
+    		printf("Welcome %s to NongGipsy Pub", name);
+  	else
+    		printf("You shall not pass!");
 	return 0;
 }
