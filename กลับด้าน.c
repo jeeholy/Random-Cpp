fe4eb5bb6a84c3287e0defb3b99fc1d6090b6769
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-27 10:47:08 +0700

    Update กลับด้าน.c

--- กลับด้าน.c
+++ กลับด้าน.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-  char a[100];
-  scanf("%s", &a);
-  for(int i=strlen(a)-1; i>=0;i--) printf("%c", a[i]);
+	char a[100];
+  	scanf("%s", &a);
+  	for(int i=strlen(a)-1; i>=0;i--) printf("%c", a[i]);
 	return 0;
 }
