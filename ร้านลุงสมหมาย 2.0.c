d8f440d2270b37be908bbd88cca4e1202d318f49
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-27 11:06:47 +0700

    Update and rename ร้านลุงสมหมาย 2.0 to ร้านลุงสมหมาย 2.0.c

--- ร้านลุงสมหมาย 2.0.c
+++ ร้านลุงสมหมาย 2.0.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main()
-{ char a[20];
-  gets(a);
-  printf("Welcome %s !", a);
+{ 
+	char a[20];
+  	gets(a);
+  	printf("Welcome %s !", a);
  	printf("\nSommai 108 Eleven Shop");
 	return 0;
 }
