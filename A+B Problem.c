eb05b6a15bd602ed81836f06deaf69b9bedc0ab6
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-27 10:43:18 +0700

    Update A+B Problem.c

--- A+B Problem.c
+++ A+B Problem.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main()
-{ int a, c, b;
-  scanf("%d", &a);
-  scanf("%d", &b);
+{	
+	int a, c, b;
+  	scanf("%d", &a);
+  	scanf("%d", &b);
 	printf("%d", a+b);
 	return 0;
 }
