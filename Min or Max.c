258e0bb4a2196c632f0ff74f9a296d5a9a126c1f
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-27 10:45:08 +0700

    Update Min or Max.c

--- Min or Max.c
+++ Min or Max.c
@@ -4,13 +4,12 @@ int main()
 {
 	int a, b;
 	scanf ("%d",&a );
-  scanf ("%d",&b );
-	
+  	scanf ("%d",&b );
 	if(a>b)
-    printf("A");
-  else if(a<b)
-    printf("B");
-  else 
-    printf("AB");
+    		printf("A");
+  	else if(a<b)
+    		printf("B");
+  	else 
+    		printf("AB");
 	return 0;
 }
