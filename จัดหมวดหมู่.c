624b7557a9470eb1682ee3e079bef12c411f84fc
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-27 10:51:22 +0700

    Update จัดหมวดหมู่.c

--- จัดหมวดหมู่.c
+++ จัดหมวดหมู่.c
@@ -3,20 +3,18 @@
 int main()
 {
 	int x, n;
-  scanf("%d %d", &x, &n);
-  
-  int f[100], t[100];
-  char a[100][25];
-  
-  for(int i=0; i<n; i++){
-    scanf("%d %d ", &f[i], &t[i]);
-    gets(a[i]);
-  }
-  for(int i=0; i<n; i++){
-    if(x>=f[i] && x<=t[i]){
-      printf("%s", a[i]);
-      break;
-    }
-  }
+  	scanf("%d %d", &x, &n);
+  	int f[100], t[100];
+  	char a[100][25];
+  	for(int i=0; i<n; i++){
+    		scanf("%d %d ", &f[i], &t[i]);
+    		gets(a[i]);
+  	}
+  	for(int i=0; i<n; i++){
+    		if(x>=f[i] && x<=t[i]){
+      			printf("%s", a[i]);
+      			break;
+    		}
+  	}
 	return 0;
 }
